542d7d78652d9ef8bbb2256fd368ca517071f814
manojnair82 <32214457+manojnair82@users.noreply.github.com>
2018-04-16 00:10:50 -0700

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,18 @@
+// add this file to .gitignore
+
+
+module.exports = {
+        google: {
+            clientID: '829741677529-38ef03tptqskt93n9rlhog4fuufacve6.apps.googleusercontent.com',
+            clientSecret: '9WZwMUCP4jyDx0H4bTLTj13o'
+        },
+        mongodb: {
+            dbURI: 'localhost:27017/reactreadinglist'
+
+            // 'localhost:27017'
+            // mongodb://<dbuser>:<ucbx1234>@ds237389.mlab.com:37389/uproot
+        },
+        session: {
+        	cookieKey: 'uprootbrowserkey'
+        }
+};
--- server.js
+++ server.js
@@ -2,8 +2,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
+const passportSetup = require('./config/passport-setup');
+const keys = require('./config/keys');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+app.use(cookieSession({
+	maxAge: 24*60*60*1000,
+	keys: [keys.session.cookieKey]
+}))
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,6 +29,11 @@ app.use(bodyParser.json());
 // Serve up static assets
 app.use(express.static("client/build"));
 // Add routes, both API and view
+
+
+
+app.use('/auth/', authRoutes);
+app.use('/profile', profileRoutes);
 app.use(routes);
 
 // Set up promises with mongoose
@@ -21,7 +44,9 @@ mongoose.connect(
   {
     useMongoClient: true
   }
-);
+).then((connected) => {
+	console.log('connected to local mongodb');
+});
 
 // Start the API server
 app.listen(PORT, function() {

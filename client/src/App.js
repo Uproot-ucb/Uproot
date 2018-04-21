9bbf435ccd6d65aa58ca62dc0807cf646464690c
manojnair82 <32214457+manojnair82@users.noreply.github.com>
2018-04-20 23:20:43 -0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Issues from "./pages/Issues";
-import IssuesReported from "./pages/IssuesReported";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import Login from "./pages/Login";
 import Users from "./pages/Users";
 import DetailUsers from "./pages/DetailUsers";
+import D3graph from "./pages/D3graph";
+
 
 const App = () => (
   <Router>
@@ -16,11 +17,12 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/issues" component={Issues} />
-        <Route exact path="/IssuesReported" component={IssuesReported} />
         <Route exact path="/issues/:id" component={Detail} />
         <Route exact path="/users" component={Users} />
         <Route exact path="/users/:id" component={DetailUsers} />
+        <Route exact path="/D3graph" component={D3graph} />
         <Route component={NoMatch} />
+
       </Switch>
     </div>
   </Router>
